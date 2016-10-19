adb886fde3ef7b3a8752ef99fa9fce12486e35a2
ashutosh.jha <ashutosh.jha@mail.vinove.com>
2016-10-19 12:43:36 +0530

    deleted extra files

--- server/app.js
+++ server/app.js
@@ -1,32 +1,24 @@
 var express = require('express');
-var path = require('path'); //remove on final
 var logger = require('morgan');//remove on final
 var bodyParser = require('body-parser');
-
 var USER = require('./model/userSchema');
 var mongoose = require('mongoose');
-
-
 var FACEBOOK_APP_ID = '370960789960977';
 var FACEBOOK_APP_SECRET='e12daa5e8407332c09848499c8d54edc';
-
-
 var SECRET = 'server secret';
-
 var expressJwt = require('express-jwt');
 var jwt = require('jsonwebtoken');
 var passport = require('passport');
 var Strategy = require('passport-local');
+
 var authenticate = expressJwt({
   secret: SECRET
 });
-
 
 mongoose.connect('mongodb://localhost/passport',function(err){
     if (err) throw err;
     else console.log('Mongo db connnected');
 });
-
 
 var app = express();
 
@@ -91,20 +83,12 @@ function authenticateCredentials(usr,psw,cb){
     })
 }
 
-// app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-
-app.set('views', path.join(__dirname, 'views'));//remove on final
-app.set('view engine', 'jade');//remove on final
-
 app.use(logger('dev'));//remove on final
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));//remove on final
-
 app.use(passport.initialize());
-//app.use(passport.session());
-
 require('./routes/api')(app, passport , Strategy , authenticate);
-
 app.listen(3000);
+
+
 module.exports = app;
--- server/routes/api.js
+++ server/routes/api.js
@@ -2,16 +2,11 @@ var API = require('../API/apiImplementation.js');
 
 
 module.exports = function (app , passport , Strategy , authenticate) {
-  app.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
-  });
-
+  app.get('/', function (req, res, next) {res.send('Working');  });
   app.post('/addnewusr',API.addnewusr);
   app.post('/loginusr', passport.initialize(), passport.authenticate('local', { session: false,scope: []}), API.loginusr);
   app.get('/pvtdata', authenticate, API.profile);
-  app.post('/fbLogin',API.fbSignup);  
-  // app.post('/fbLogin',passport.authenticate('facebook-token'),API.fbLogin);  
-  // app.get('/fb',API.fbLogin);  
+  app.post('/fbLogin',API.fbSignup);   
   app.post('/fbLoginAuth',passport.authenticate('facebook-token'),API.fbLoginAuth)
   app.post('/intWidFB',authenticate,API.intWidFB);
 }
